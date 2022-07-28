fab26b64e4a46276992415260de76d3e496c7c8b
rimcodes <110171099+rimcodes@users.noreply.github.com>
2022-07-28 13:50:03 +0000

    Update Post.php
    Added protected field table

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -31,6 +31,13 @@ class Post extends Model
     ];
 
     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'posts';
+    
+    /**
      * The attributes that should be mutated to dates.
      *
      * @var array
