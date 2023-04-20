fc6b63963a1448dc2efea427c093c76f9ad83d2d
Haden MV <med4en@gmail.com>
2023-04-20 23:12:11 +0000

    Added angualr app dist files directly to laravel app

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html><html lang="ar" dir="rtl"><head>
+  <meta charset="utf-8">
+  <title>تدريس | موقع تعليمي </title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <meta name="Keywords" content="تعليم البرمجة، لغة الترميز القياسي HTML, Python بايثون, CSS لغة النماط المتتالية, JavaScript جافاسكريبة, كيف يمكنك, Angular انجولر, دورات تدريبية,, Web Development , التطوير, تعليم">
+  <meta name="Description" content="دروس بناء الويب جيدة التنظيم وسهلة الفهم مع الكثير من الأمثلة حول كيفية استخدام HTML و CSS و JavaScript و SQL و Python و PHP و Bootstrap والمزيد.">
+  <meta property="og:image" content="logo.png">
+  <meta property="og:image:type" content="image/png">
+  <meta property="og:image:width" content="436">
+  <meta property="og:image:height" content="228">
+  <meta property="og:title" content="تدريس تعليم برمجة الويب بالعربية">
+  <meta property="og:description" content="يقدم موقع تدريس دروسًا تعليمية ومراجع وتمارين مجانية على الإنترنت بجميع اللغات الرئيسية على الويب. تغطي الموضوعات الشائعة مثل HTML و CSS و JavaScript و Python و SQL و Java وغيرها الكثير.">
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async="" src="https://www.googletagmanager.com/gtag/js?id=G-QEZGS9DKTR"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-QEZGS9DKTR');
+    </script>
+  <link rel="stylesheet" href="assets/fonts/fonts.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="assets/fonts/fonts.css"></noscript>
+<style>*,:before,:after{box-sizing:border-box;border-width:0;border-style:solid;border-color:#e5e7eb}:before,:after{--tw-content:""}html{line-height:1.5;-webkit-text-size-adjust:100%;tab-size:4;font-family:ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,"Apple Color Emoji","Segoe UI Emoji",Segoe UI Symbol,"Noto Color Emoji";font-feature-settings:normal;font-variation-settings:normal}body{margin:0;line-height:inherit}html{scroll-behavior:smooth}body{direction:rtl;font-family:Nunito,sans-serif;--tw-bg-opacity:1;background-color:rgb(238 238 238 / var(--tw-bg-opacity));--tw-text-opacity:1;color:rgb(34 40 49 / var(--tw-text-opacity));transition-property:color,background-color,border-color,text-decoration-color,fill,stroke,opacity,box-shadow,transform,filter,-webkit-backdrop-filter;transition-property:color,background-color,border-color,text-decoration-color,fill,stroke,opacity,box-shadow,transform,filter,backdrop-filter;transition-property:color,background-color,border-color,text-decoration-color,fill,stroke,opacity,box-shadow,transform,filter,backdrop-filter,-webkit-backdrop-filter;transition-timing-function:cubic-bezier(.4,0,.2,1);transition-duration:.5s}*,:before,:after{--tw-border-spacing-x:0;--tw-border-spacing-y:0;--tw-translate-x:0;--tw-translate-y:0;--tw-rotate:0;--tw-skew-x:0;--tw-skew-y:0;--tw-scale-x:1;--tw-scale-y:1;--tw-pan-x: ;--tw-pan-y: ;--tw-pinch-zoom: ;--tw-scroll-snap-strictness:proximity;--tw-ordinal: ;--tw-slashed-zero: ;--tw-numeric-figure: ;--tw-numeric-spacing: ;--tw-numeric-fraction: ;--tw-ring-inset: ;--tw-ring-offset-width:0px;--tw-ring-offset-color:#fff;--tw-ring-color:rgb(59 130 246 / .5);--tw-ring-offset-shadow:0 0 #0000;--tw-ring-shadow:0 0 #0000;--tw-shadow:0 0 #0000;--tw-shadow-colored:0 0 #0000;--tw-blur: ;--tw-brightness: ;--tw-contrast: ;--tw-grayscale: ;--tw-hue-rotate: ;--tw-invert: ;--tw-saturate: ;--tw-sepia: ;--tw-drop-shadow: ;--tw-backdrop-blur: ;--tw-backdrop-brightness: ;--tw-backdrop-contrast: ;--tw-backdrop-grayscale: ;--tw-backdrop-hue-rotate: ;--tw-backdrop-invert: ;--tw-backdrop-opacity: ;--tw-backdrop-saturate: ;--tw-backdrop-sepia: }</style><link rel="stylesheet" href="styles.81f81389f92bbd5d.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles.81f81389f92bbd5d.css"></noscript></head>
+<body>
+  <app-root></app-root>
+<script src="runtime.f70b245b35c75e5b.js" type="module"></script><script src="polyfills.5fd626fde791e8c3.js" type="module"></script><script src="main.e69a7515bdd03c46.js" type="module"></script>
+
+</body></html>
