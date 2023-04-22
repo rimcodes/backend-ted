295056474d6e026a51cbc3939a1c1f1c1bd67c77
Haden MV <med4en@gmail.com>
2023-04-22 18:14:22 +0000

    Added seeders for admin user

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::domain('admin.' . env('APP_URL'))->group(function () {
+//     Route::get('/', function () {
+//         return view('home');
+//     });
+
+// });
 Route::get('/', function () {
     return view('home');
 });
+
